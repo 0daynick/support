666c39475e03a0b06527471f2e345beecb033cb1
overnick <overnic@httpd.cc>
2018-04-12 10:23:40 +0800

    v1.0.0

---
+++ Contracts/Arrayable.php
@@ -0,0 +1,11 @@
+<?php
+namespace OverNick\Support\Contracts;
+interface Arrayable
+{
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray();
+}
