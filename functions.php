20292ff86c86f285361dd768f1787705d8f06bf4
overnick <overnic@httpd.cc>
2018-04-14 17:28:25 +0800

    add get_client_ip and get_server_ip functions

--- functions.php
+++ functions.php
@@ -12,3 +12,43 @@ if (! function_exists('value')) {
         return $value instanceof Closure ? $value() : $value;
     }
 }
+
+if (! function_exists('get_client_ip')) {
+    /**
+     * Get client ip.
+     *
+     * @return string
+     */
+    function get_client_ip()
+    {
+        if (!empty($_SERVER['REMOTE_ADDR'])) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        } else {
+            // for php-cli(phpunit etc.)
+            $ip = defined('PHPUNIT_RUNNING') ? '127.0.0.1' : gethostbyname(gethostname());
+        }
+
+        return filter_var($ip, FILTER_VALIDATE_IP) ?: '127.0.0.1';
+    }
+}
+
+if (! function_exists('get_server_ip')) {
+    /**
+     * Get current server ip.
+     *
+     * @return string
+     */
+    function get_server_ip()
+    {
+        if (!empty($_SERVER['SERVER_ADDR'])) {
+            $ip = $_SERVER['SERVER_ADDR'];
+        } elseif (!empty($_SERVER['SERVER_NAME'])) {
+            $ip = gethostbyname($_SERVER['SERVER_NAME']);
+        } else {
+            // for php-cli(phpunit etc.)
+            $ip = defined('PHPUNIT_RUNNING') ? '127.0.0.1' : gethostbyname(gethostname());
+        }
+
+        return filter_var($ip, FILTER_VALIDATE_IP) ?: '127.0.0.1';
+    }
+}
